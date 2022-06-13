9511a967fc7d3b8ce02ba0e6effe34b633a6a3d0
Lucas Glück <luglueck@unibz.it>
2022-06-13 18:04:19 +0200

    changes

--- Proj2/routes/homePage.js
+++ Proj2/routes/homePage.js
@@ -50,71 +50,45 @@ router.get('/', (req, res, next) => {
 })
 
 
-// router.get('/', (req, res, next) => {
-// 	console.log(req.query);
-	
-// 	if ('username' in req.query && 'password' in req.query) {
-// 		db.all("SELECT * FROM users WHERE username=$username AND password=$password", {$username: req.query.username, $password: req.query.password}, function(err, row) {
-// 			let curuser = row[0];
+router.get('/feed', (req, res, next) => {
+	console.log(req.query);
 
-// 			if(curuser.username == req.query.username && curuser.password == req.query.password) {
-			
-// 				fs.readFile(path.join(__dirname, '..', 'views', 'homePage.html'), 'utf8', (err, page) => {
-// 					if (err) {
-// 						console.error(err);
-// 						return;
-// 					}
+	db.all("SELECT * FROM allPosts", function(err, rows) {
 
-// 					page = page.replace('{%name%}', curuser.name)
-// 					page = page.replace('{%surname%}', curuser.surname)
-// 					page = page.replace('{%username%}', curuser.username)
-// 					page = page.replace('{%password%}', curuser.password)
-// 					page = page.replace('{%description%}', curuser.description)
-// 				});
-// 			} else {
-// 				res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'))
-// 			}
-// 		})
-
-
-// 		db.all("SELECT * FROM allPosts", function(err, rows) {
-
-// 			fs.readFile(path.join(__dirname, '..', 'views', 'homePage.html'), 'utf8', (err, page) => {
-// 				if (err) {
-// 					console.error(err);
-// 					return;
-// 				}
-// 				let table_html = ''
-// 				rows.forEach(
-// 					el => table_html = table_html + 
-// 					'<div class="container">' +
-// 					'<div class="container" id="container2">' +
-// 						'<div class="col-sm-12 col-md-8 col-lg-8" style= "padding:3%;">' +
-// 							'<div class="">' +
-// 								'<h2 style="color:black !important">' + el['username'] + '</h2>' +
-// 								'<p style="color:black !important">' + el['posts'] +  '</p>' +
-// 								'<button class="button">' +
-// 									'<a href="">like</a>' +
-// 								'</button>' +
-// 								'<button class="button">' +
-// 									'<a href="">dislike</a>' +
-// 								'</button>' +
-// 							'</div>' +
-// 						'</div>' +
-// 					'</div>'
-// 				);
-// 				page = page.replace('{%feed%}', table_html);
-// 				res.write(page);
+		fs.readFile(path.join(__dirname, '..', 'views', 'homePage.html'), 'utf8', (err, page) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			let table_html = ''
+			rows.forEach(
+				el => table_html = table_html + 
+				'<div class="container">' +
+					'<div class="container" id="container2">' +
+						'<div class="col-sm-12 col-md-8 col-lg-8" style= "padding:3%;">' +
+							'<div class="">' +
+								'<h2 style="color:black !important">' + el['username'] + '</h2>' +
+								'<p style="color:black !important">' + el['posts'] +  '</p>' +
+								'<button class="button">' +
+									'<a href="">like</a>' +
+								'</button>' +
+								'<button class="button">' +
+									'<a href="">dislike</a>' +
+								'</button>' +
+							'</div>' +
+						'</div>' +
+					'</div>' +
+				'</div>'
+				);
+				page = page.replace('{%feed%}', table_html);
+				res.write(page);
 					
-// 				res.end();
-// 			});
+				res.end();
+			});
 				
-// 		});
-// 	} else {	
-// 		res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'))
-// 	}
+		});
 
-// })
+})
 
 
 router.post('/newPost', urlencodedParser, (req, res) => {
